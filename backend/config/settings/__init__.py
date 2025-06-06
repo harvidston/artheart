5aa62168b033cf56ca74f5c082a2806b3971db91
harvidston <harvidston@mail.ru>
2025-06-07 02:11:41 +0300

    chore: initial Django project setup
    Add base project structure with manage.py, settings, .gitignore and README.md

---
+++ backend/config/settings/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+env = os.getenv('DJANGO_ENV', 'dev')
+
+if env == 'prod':
+    from .prod import *
+else:
+    from .dev import *
+
---
+++ backend/config/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
